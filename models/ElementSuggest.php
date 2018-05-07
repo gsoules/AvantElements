c12a11eae02aefbcbc6ad9a524dbcd5c0a7cb5ad
gsoules <gsoules@avantlogic.com>
2018-05-07 17:45:27 -0400

    Continue development of Suggest feature.

--- models/ElementSuggest.php
+++ models/ElementSuggest.php
@@ -6,59 +6,13 @@ class ElementSuggest
     {
         $term = $_GET['term'];
         $customCallback = new CustomCallback();
-        $suggestions = $customCallback->performCallbackForElement(CustomCallback::CALLBACK_ACTION_SUGGEST, null, $elementId, $term);
 
-        //$suggestions = call_user_func('DigitalArchive::suggestTitle', null, $elementId, $term);
+        $suggestions = $customCallback->performCallbackForElement(CustomCallback::CALLBACK_ACTION_SUGGEST, null, $elementId, trim($term));
 
-        //$suggestions = $this->searchTitles($term);
         if (empty($suggestions))
         {
             $suggestions = array("No suggestions for '$term'");
         }
         return json_encode($suggestions);
-    }
-
-    public function searchTitles($term)
-    {
-        $words = explode(' ', $term);
-        $words = array_map('trim', $words);
-        $query = '';
-        foreach ($words as $word)
-        {
-            if (empty($word) || SearchQueryBuilder::isStopWord($word))
-            {
-                continue;
-            }
-            if (!empty($query))
-            {
-                $query .= ' ';
-            }
-            $query .= "+$word*";
-        }
-
-        $maxResults = 25;
-
-        $db = get_db();
-        $select = $db->select()
-            ->from($db->SearchTexts, array('title'))
-            ->where("MATCH (title) AGAINST ('$query' IN BOOLEAN MODE)")
-            ->limit($maxResults)
-            ->order('title');
-
-        $results = $db->getTable('ElementText')->fetchObjects($select);
-
-        $suggestions = array();
-        foreach ($results as $result)
-        {
-            // Account for the fact that when an item has more than one title, each title value is separated in the
-            // search_texts table's title column with a special delimiter. This code returns each of the titles.
-            $titles = explode('||', $result->title);
-            foreach ($titles as $title)
-            {
-                $suggestions[] = $title;
-            }
-        }
-
-        return $suggestions;
     }
 }
