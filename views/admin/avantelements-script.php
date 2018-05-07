04977d1ba91b6b33dae385736ae165a98888138f
gsoules <gsoules@avantlogic.com>
2018-05-07 12:41:12 -0400

    Refinements to jQuery.

--- views/admin/avantelements-script.php
+++ views/admin/avantelements-script.php
@@ -1,15 +1,19 @@
 <script type="text/javascript">
     jQuery(document).ready(function ()
     {
-        jQuery('#Elements-39-0-text').autocomplete({
-            source: '<?php echo url('/elements/suggest/39'); ?>',
-            minLength: 2
-        });
+        for (i=39; i<=41; i++)
+        {
+            jQuery('#Elements-' + i + '-0-text').autocomplete({
+                source: '<?php echo url('/elements/suggest/'); ?>' + i,
+                minLength: 2
+            });
+        }
 
-        jQuery('#Elements-41-0-text').autocomplete({
-            source: '<?php echo url('/elements/suggest'); ?>',
-            minLength: 2
-        });
+
+        //jQuery('#Elements-41-0-text').autocomplete({
+        //    source: '<?php //echo url('/elements/suggest'); ?>//',
+        //    minLength: 2
+        //});
 
         jQuery('#clone-button').appendTo('#edit');
     });
