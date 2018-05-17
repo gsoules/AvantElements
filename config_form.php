54c4f36f5f2f72e9815270a739ff922aa53f9fe5
gsoules <gsoules@avantlogic.com>
2018-05-17 11:24:49 -0400

    Fix bug where wrong class referenced.

--- config_form.php
+++ config_form.php
@@ -146,7 +146,7 @@ $titleSyncOptionRows = max(2, count(explode(PHP_EOL, $titleSyncOption)));
             <p class="explanation"><?php echo __("SimpleVocab elements that display as a dropdown list."); ?></p>
             <?php echo $view->formTextarea(ElementsConfig::OPTION_SELECT_FIELD, $selectField, array('rows' => $selectFieldRows)); ?>
         <?php else: ?>
-            <?php SearchConfig::emitOptionNotSupported('AvantElements', 'simple-vocab'); ?>
+            <?php ElementsConfig::emitOptionNotSupported('AvantElements', 'simple-vocab'); ?>
         <?php endif; ?>
     </div>
 </div>
