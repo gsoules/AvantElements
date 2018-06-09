f86cb8f5c6dc835750c36a21b84a0e0eb461335c
gsoules <gsoules@avantlogic.com>
2018-06-09 18:58:46 -0400

    Fix but where Suggest did not work with multiple inputs for the same element.

--- views/admin/avantelements-script.php
+++ views/admin/avantelements-script.php
@@ -1,17 +1,27 @@
 <script type="text/javascript">
     var fields = [<?php echo $fields; ?>];
 
-    jQuery(document).ready(function ()
+    function enableAutoComplete()
     {
-        for (i = 0; i < fields.length; i++)
-        {
+        for (i = 0; i < fields.length; i++) {
             var elementId = fields[i];
-            jQuery('[id^=Elements-' + elementId + ']').autocomplete({
-                source: '<?php echo url('/elements/suggest/'); ?>' + elementId,
-                minLength: 2
+            jQuery('[id^=Elements-' + elementId + ']').each(function () {
+                var d = new Date();
+                var n = d.getTime();
+                console.log(n + ' ' + this.id);
+                jQuery(this).autocomplete({
+                    source: '<?php echo url('/elements/suggest/'); ?>' + elementId,
+                    minLength: 2
+                });
             });
         }
+    }
 
-        jQuery('#clone-button').appendTo('#edit');
+    jQuery(document).bind('omeka:elementformload', function (event) {
+        console.log("LOAD");
+        enableAutoComplete();
     });
+
+
+    jQuery('#clone-button').appendTo('#edit');
 </script>
