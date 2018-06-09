06f2df9a411ec7410b59103ce0874e2b45c0ace9
gsoules <gsoules@avantlogic.com>
2018-06-09 19:07:49 -0400

    Cleanup code for auto-complete.

--- views/admin/avantelements-script.php
+++ views/admin/avantelements-script.php
@@ -1,14 +1,14 @@
 <script type="text/javascript">
     var fields = [<?php echo $fields; ?>];
 
+    // Enable auto-complete for each instance of each element having an Id in the fields list.
+    // If the user has clicked the Add Input button, the form will reload and replace the existing
+    // inputs for an element with those plus the new one. This function adds auto-complete to each.
     function enableAutoComplete()
     {
         for (i = 0; i < fields.length; i++) {
             var elementId = fields[i];
             jQuery('[id^=Elements-' + elementId + ']').each(function () {
-                var d = new Date();
-                var n = d.getTime();
-                console.log(n + ' ' + this.id);
                 jQuery(this).autocomplete({
                     source: '<?php echo url('/elements/suggest/'); ?>' + elementId,
                     minLength: 2
@@ -17,11 +17,11 @@
         }
     }
 
+    // Handle both an initial form load, and when the form is updated after the Add Input button is clicked.
     jQuery(document).bind('omeka:elementformload', function (event) {
-        console.log("LOAD");
         enableAutoComplete();
     });
 
-
+    // Move the Duplicate Item button to the end of the set of other buttons.
     jQuery('#clone-button').appendTo('#edit');
 </script>
